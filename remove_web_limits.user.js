5be4c36dec376b22dd14f425dd09959ef5f1c488
Cat73 <1901803382@qq.com>
2016-12-06 11:36:36 +0800

    整理下代码

--- remove_web_limits.user.js
+++ remove_web_limits.user.js
@@ -32,206 +32,208 @@
 // @grant             none
 // @run-at            document-start
 // ==/UserScript==
-'use strict';
+(function() {
+  'use strict';
 
-// 域名规则列表
-var rules = {
-  black_rule: {
-    name: "black",
-    hook_eventNames: "",
-    unhook_eventNames: ""
-  },
-  default_rule: {
-    name: "default",
-    hook_eventNames: "contextmenu|select|selectstart|copy|cut|dragstart",
-    unhook_eventNames: "mousedown|mouseup|keydown|keyup",
-    dom0: true,
-    hook_addEventListener: true,
-    hook_preventDefault: true,
-    hook_set_returnValue: true,
-    add_css: true
-  }
-};
-// 域名列表
-var lists = {
-  // 黑名单
-  black_list: [
-    /.*\.youtube\.com.*/,
-    /.*\.wikipedia\.org.*/,
-    /mail\.qq\.com.*/,
-    /translate\.google\..*/
-  ]
-};
-
-// 要处理的 event 列表
-var hook_eventNames, unhook_eventNames, eventNames;
-// 储存名称
-var storageName = getRandStr('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM', parseInt(Math.random() * 12 + 8));
-// 储存被 Hook 的函数
-var EventTarget_addEventListener = EventTarget.prototype.addEventListener;
-var document_addEventListener = document.addEventListener;
-var Event_preventDefault = Event.prototype.preventDefault;
-
-// Hook addEventListener proc
-function addEventListener(type, func, useCapture) {
-  var _addEventListener = this === document ? document_addEventListener : EventTarget_addEventListener;
-  if(hook_eventNames.indexOf(type) >= 0) {
-    _addEventListener.apply(this, [type, returnTrue, useCapture]);
-  } else if(unhook_eventNames.indexOf(type) >= 0) {
-    var funcsName = storageName + type + (useCapture ? 't' : 'f');
-
-    if(this[funcsName] === undefined) {
-      this[funcsName] = [];
-      _addEventListener.apply(this, [type, useCapture ? unhook_t : unhook_f, useCapture]);
+  // 域名规则列表
+  var rules = {
+    black_rule: {
+      name: "black",
+      hook_eventNames: "",
+      unhook_eventNames: ""
+    },
+    default_rule: {
+      name: "default",
+      hook_eventNames: "contextmenu|select|selectstart|copy|cut|dragstart",
+      unhook_eventNames: "mousedown|mouseup|keydown|keyup",
+      dom0: true,
+      hook_addEventListener: true,
+      hook_preventDefault: true,
+      hook_set_returnValue: true,
+      add_css: true
     }
+  };
+  // 域名列表
+  var lists = {
+    // 黑名单
+    black_list: [
+      /.*\.youtube\.com.*/,
+      /.*\.wikipedia\.org.*/,
+      /mail\.qq\.com.*/,
+      /translate\.google\..*/
+    ]
+  };
 
-    this[funcsName].push(func)
-  } else {
-    _addEventListener.apply(this, arguments);
+  // 要处理的 event 列表
+  var hook_eventNames, unhook_eventNames, eventNames;
+  // 储存名称
+  var storageName = getRandStr('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM', parseInt(Math.random() * 12 + 8));
+  // 储存被 Hook 的函数
+  var EventTarget_addEventListener = EventTarget.prototype.addEventListener;
+  var document_addEventListener = document.addEventListener;
+  var Event_preventDefault = Event.prototype.preventDefault;
+
+  // Hook addEventListener proc
+  function addEventListener(type, func, useCapture) {
+    var _addEventListener = this === document ? document_addEventListener : EventTarget_addEventListener;
+    if(hook_eventNames.indexOf(type) >= 0) {
+      _addEventListener.apply(this, [type, returnTrue, useCapture]);
+    } else if(unhook_eventNames.indexOf(type) >= 0) {
+      var funcsName = storageName + type + (useCapture ? 't' : 'f');
+
+      if(this[funcsName] === undefined) {
+        this[funcsName] = [];
+        _addEventListener.apply(this, [type, useCapture ? unhook_t : unhook_f, useCapture]);
+      }
+
+      this[funcsName].push(func);
+    } else {
+      _addEventListener.apply(this, arguments);
+    }
   }
-}
 
-// 清理循环
-function clearLoop() {
-  var elements = getElements();
+  // 清理循环
+  function clearLoop() {
+    var elements = getElements();
 
-  for(var i in elements) {
-    for(var j in eventNames) {
-      var name = 'on' + eventNames[j];
-      if(elements[i][name] != null && elements[i][name] != onxxx) {
-        if(unhook_eventNames.indexOf(eventNames[j]) >= 0) {
-          elements[i][storageName + name] = elements[i][name];
-          elements[i][name] = onxxx;
-        } else {
-          elements[i][name] = null;
+    for(var i in elements) {
+      for(var j in eventNames) {
+        var name = 'on' + eventNames[j];
+        if(elements[i][name] !== null && elements[i][name] !== onxxx) {
+          if(unhook_eventNames.indexOf(eventNames[j]) >= 0) {
+            elements[i][storageName + name] = elements[i][name];
+            elements[i][name] = onxxx;
+          } else {
+            elements[i][name] = null;
+          }
         }
       }
     }
   }
-}
 
-// 返回true的函数
-function returnTrue(e) {
-  return true;
-}
-function unhook_t(e) {
-  return unhook(e, this, storageName + e.type + 't');
-}
-function unhook_f(e) {
-  return unhook(e, this, storageName + e.type + 'f');
-}
-function unhook(e, self, funcsName) {
-  var list = self[funcsName];
-  for(var i in list) {
-    list[i](e);
+  // 返回true的函数
+  function returnTrue(e) {
+    return true;
   }
-
-  e.returnValue = true;
-  return true;
-}
-function onxxx(e) {
-  var name = storageName + 'on' + e.type;
-  this[name](e);
-
-  e.returnValue = true;
-  return true;
-}
-
-// 获取随机字符串
-function getRandStr(chs, len) {
-  var str = '';
-
-  while(len--) {
-    str += chs[parseInt(Math.random() * chs.length)];
+  function unhook_t(e) {
+    return unhook(e, this, storageName + e.type + 't');
   }
-
-  return str;
-}
-
-// 获取所有元素 包括document
-function getElements() {
-  var elements = Array.prototype.slice.call(document.getElementsByTagName('*'));
-  elements.push(document);
-
-  return elements;
-}
-
-// 添加css
-function addStyle(css) {
-  var style = document.createElement('style');
-  style.innerHTML = css;
-  document.head.appendChild(style);
-}
-
-// 获取目标域名应该使用的规则
-function getRule(url) {
-  function testUrl(list, url) {
+  function unhook_f(e) {
+    return unhook(e, this, storageName + e.type + 'f');
+  }
+  function unhook(e, self, funcsName) {
+    var list = self[funcsName];
     for(var i in list) {
-      if(list[i].test(url)) {
-        return true;
-      }
+      list[i](e);
     }
 
-    return false;
+    e.returnValue = true;
+    return true;
+  }
+  function onxxx(e) {
+    var name = storageName + 'on' + e.type;
+    this[name](e);
+
+    e.returnValue = true;
+    return true;
   }
 
-  if(testUrl(lists.black_list, url)) {
-    return rules.black_rule;
+  // 获取随机字符串
+  function getRandStr(chs, len) {
+    var str = '';
+
+    while(len--) {
+      str += chs[parseInt(Math.random() * chs.length)];
+    }
+
+    return str;
   }
 
-  return rules.default_rule;
-}
+  // 获取所有元素 包括document
+  function getElements() {
+    var elements = Array.prototype.slice.call(document.getElementsByTagName('*'));
+    elements.push(document);
 
-// 初始化
-function init() {
-  // 获取当前域名的规则
-  var url = window.location.host + window.location.pathname;
-  var rule = getRule(url);
-
-  // 设置 event 列表
-  hook_eventNames = rule.hook_eventNames.split("|");
-  // TODO Allowed to return value
-  unhook_eventNames = rule.unhook_eventNames.split("|");
-  eventNames = hook_eventNames.concat(unhook_eventNames);
-
-  // 调用清理 DOM0 event 方法的循环
-  if(rule.dom0) {
-    setInterval(clearLoop, 30 * 1000);
-    setTimeout(clearLoop, 2500);
-    window.addEventListener('load', clearLoop, true);
-    clearLoop();
+    return elements;
   }
 
-  // hook addEventListener
-  if(rule.hook_addEventListener) {
-    EventTarget.prototype.addEventListener = addEventListener;
-    document.addEventListener = addEventListener;
+  // 添加css
+  function addStyle(css) {
+    var style = document.createElement('style');
+    style.innerHTML = css;
+    document.head.appendChild(style);
   }
 
-  // hook preventDefault
-  if(rule.hook_preventDefault) {
-    Event.prototype.preventDefault = function() {
-      if(eventNames.indexOf(this.type) < 0) {
-        Event_preventDefault.apply(this, arguments);
+  // 获取目标域名应该使用的规则
+  function getRule(url) {
+    function testUrl(list, url) {
+      for(var i in list) {
+        if(list[i].test(url)) {
+          return true;
+        }
       }
-    };
+
+      return false;
+    }
+
+    if(testUrl(lists.black_list, url)) {
+      return rules.black_rule;
+    }
+
+    return rules.default_rule;
   }
 
-  // Hook set returnValue
-  if(rule.hook_set_returnValue) {
-    Event.prototype.__defineSetter__('returnValue', function() {
-      if(this.returnValue != true && eventNames.indexOf(this.type) >= 0) {
-        this.returnValue = true;
-      }
-    });
+  // 初始化
+  function init() {
+    // 获取当前域名的规则
+    var url = window.location.host + window.location.pathname;
+    var rule = getRule(url);
+
+    // 设置 event 列表
+    hook_eventNames = rule.hook_eventNames.split("|");
+    // TODO Allowed to return value
+    unhook_eventNames = rule.unhook_eventNames.split("|");
+    eventNames = hook_eventNames.concat(unhook_eventNames);
+
+    // 调用清理 DOM0 event 方法的循环
+    if(rule.dom0) {
+      setInterval(clearLoop, 30 * 1000);
+      setTimeout(clearLoop, 2500);
+      window.addEventListener('load', clearLoop, true);
+      clearLoop();
+    }
+
+    // hook addEventListener
+    if(rule.hook_addEventListener) {
+      EventTarget.prototype.addEventListener = addEventListener;
+      document.addEventListener = addEventListener;
+    }
+
+    // hook preventDefault
+    if(rule.hook_preventDefault) {
+      Event.prototype.preventDefault = function() {
+        if(eventNames.indexOf(this.type) < 0) {
+          Event_preventDefault.apply(this, arguments);
+        }
+      };
+    }
+
+    // Hook set returnValue
+    if(rule.hook_set_returnValue) {
+      Event.prototype.__defineSetter__('returnValue', function() {
+        if(this.returnValue !== true && eventNames.indexOf(this.type) >= 0) {
+          this.returnValue = true;
+        }
+      });
+    }
+
+    console.debug('url: ' + url, 'storageName：' + storageName, 'rule: ' + rule.name);
+
+    // 添加CSS
+    if(rule.add_css) {
+      addStyle('html, * {-webkit-user-select:text!important; -moz-user-select:text!important;}');
+    }
   }
 
-  console.debug('url: ' + url, 'storageName：' + storageName, 'rule: ' + rule.name);
-
-  // 添加CSS
-  if(rule.add_css) {
-    addStyle('html, * {-webkit-user-select:text!important; -moz-user-select:text!important;}');
-  }
-}
-
-init();
+  init();
+})();
